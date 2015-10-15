8a5c02f009fae1cdb91d7eee9a9d61f07a3b2b8b
Vlad Mazur <vladmazur93@gmail.com>
2015-10-15 20:18:11 +0300

    New Tree (30 elements)

--- BinaryTree/main.cpp
+++ BinaryTree/main.cpp
@@ -5,16 +5,16 @@
 int main()
 {
 	BinaryTree *myTree = new BinaryTree;
+/*
+myTree->AddNewElem(5, 5);
+myTree->AddNewElem(2, 2);
+myTree->AddNewElem(3, 3);
+myTree->AddNewElem(1, 1);
+myTree->AddNewElem(8, 8);
+myTree->AddNewElem(6, 6);
+myTree->AddNewElem(7, 7);
+myTree->AddNewElem(9, 9);
 
-	myTree->AddNewElem(5, 5);
-	myTree->AddNewElem(2, 2);
-	myTree->AddNewElem(3, 3);	
-	myTree->AddNewElem(1, 1);	
-	myTree->AddNewElem(8, 8);
-	myTree->AddNewElem(6, 6);
-	myTree->AddNewElem(7, 7);
-	myTree->AddNewElem(9, 9);
-	
 //	myTree->FindElByKey(1);
 //	myTree->FindElByData(1);
 
@@ -22,8 +22,39 @@ int main()
 
 //	myTree->FindElByDataRec(1);
 //	myTree->FindElByDataBreadth(3);				//	работает
+*/
+	myTree->AddNewElem(23, 5);
+	myTree->AddNewElem(42, 5);
+	myTree->AddNewElem(15, 5);
+	myTree->AddNewElem(19, 5);
+	myTree->AddNewElem(21, 5);
+	myTree->AddNewElem(20, 5);
+	myTree->AddNewElem(31, 5);
+	myTree->AddNewElem(49, 5);
+	myTree->AddNewElem(45, 5);
+	myTree->AddNewElem(8, 5);
+	myTree->AddNewElem(12, 5);
+	myTree->AddNewElem(4, 5);
+	myTree->AddNewElem(17, 5);
+	myTree->AddNewElem(10, 5);
+	myTree->AddNewElem(16, 5);
+	myTree->AddNewElem(18, 5);
+	myTree->AddNewElem(2, 5);
+	myTree->AddNewElem(6, 5);
+	myTree->AddNewElem(13, 5);
+	myTree->AddNewElem(27, 5);
+	myTree->AddNewElem(36, 5);
+	myTree->AddNewElem(39, 5);
+	myTree->AddNewElem(37, 5);
+	myTree->AddNewElem(40, 5);
+	myTree->AddNewElem(30, 5);
+	myTree->AddNewElem(28, 5);
+	myTree->AddNewElem(29, 5);
+	myTree->AddNewElem(41, 5);
+	myTree->AddNewElem(51, 5);
+	myTree->AddNewElem(50, 5);
 
-	myTree->DeleteElByKey(1);
+	//myTree->DeleteElByKey(1);
 
 	_getch();
 	return 0;
